2e2c467b9c3b7cdd8a8502f2354dd9a1a6b07c2e
Jade Meskill <jade@integrumtech.com>
2013-09-25 21:53:38 -0700

    add config options and transcription

--- post_produce.sh
+++ post_produce.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-source config.sh
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+CONFIG_FILE=config.sh
+BLOG_POST_JSON_FILE=blog_post.json
+
+while getopts ":c:j:" opt; do
+    case "$opt" in
+    c)
+      CONFIG_FILE=$OPTARG
+      ;;
+
+    j)
+      BLOG_POST_JSON_FILE=$OPTARG
+      ;;
+
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+source $CONFIG_FILE
 
 SOURCE_FILE=$1
 
@@ -9,7 +33,6 @@ TMP_FILE2=/tmp/out_2.wav
 TMP_FILE3=/tmp/out_3.wav
 TMP_FILE4=/tmp/out_3.output.wav
 OUTPUT_MP3_FILE=`echo "$SOURCE_FILE" | sed -e s/\.wav//`.mp3
-
 
 rm -f $TMP_FILE1 $TMP_FILE2 $TMP_FILE3 $TMP_FILE4
 
@@ -22,11 +45,11 @@ sox --combine concatenate $TMP_FILE2 $OUTRO_WAV_FILE $TMP_FILE3
 lame $TMP_FILE4 $OUTPUT_MP3_FILE
 
 echo "uploading to s3"
-podcast_url=`s3cmd --config=$S3_CONFIG_FILE put --acl-public --guess-mime-type $OUTPUT_MP3_FILE s3://$S3_BUCKET | grep -o 'http.*'`
-echo "S3 url: $podcast_url"
+PODCAST_URL=`s3cmd --config=$S3_CONFIG_FILE put --acl-public --guess-mime-type $OUTPUT_MP3_FILE s3://$S3_BUCKET | grep -o 'http.*'`
+echo "S3 url: $PODCAST_URL"
 
 echo "posting to Wordpress"
-phantomjs post_blog.coffee "`cat blog_post.json`" "$podcast_url" | tee phantom_result.txt
+phantomjs post_blog.coffee "`cat $BLOG_POST_JSON_FILE`" "$PODCAST_URL" | tee phantom_result.txt
 # -------
 
 phantom_result=`cat phantom_result.txt`
@@ -47,7 +70,11 @@ bitly_link=`expr "$bitly_json" : '.*"url": "\([^"]*\)"'`
 bitly_link=${bitly_link//"\/"/"/"}
 echo "bit.ly link is $bitly_link"
 
+echo "ordering transcription"
+curl --data "{\"api_key\":\"${CASTING_WORDS_API_KEY}\", \"url\":\"${PODCAST_URL}\", \"sku\":\"BULK14\"}" https://castingwords.com/store/API4/order_url -H "Content-Type: application/json" | tee transcript.json
+
+
 echo "scheduling tweet"
-phantomjs schedule_tweet.coffee "`cat blog_post.json`" "$bitly_link"
+phantomjs schedule_tweet.coffee "`cat $BLOG_POST_JSON_FILE`" "$bitly_link"
 
 open /tmp/test.png
